7b39e483ba38278cf561ec9e8153e3968d11051b
norah-asiri <cs.norah2018@gmail.com>
2021-12-06 23:58:11 +0000

    update

--- ios application/TTTGameNorahAsiri/TTTGameNorahAsiri/ViewController.swift
+++ ios application/TTTGameNorahAsiri/TTTGameNorahAsiri/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var player1 : [UIButton] = []
+    var player2 : [UIButton] = []
+
+    @IBOutlet var gameButton: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    setupUI()
     }
-
+    
+    func setupUI(){
+        for aBtn in gameButton {
+            aBtn.layer.backgroundColor = UIColor.lightGray.cgColor
+        }
+    }
+    
+    @IBAction func gameAction(_ sender: UIButton) {
+        print ("My tag is \(sender.tag)")
+    
+    
+    }
+    
 
 }
 
