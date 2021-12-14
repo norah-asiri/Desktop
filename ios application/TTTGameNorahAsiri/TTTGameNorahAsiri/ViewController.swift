75f850243e28b0e54e8eb3fcb12a1a8110adb212
norah-asiri <cs.norah2018@gmail.com>
2021-12-14 06:24:16 +0000

    tttGame

--- ios application/TTTGameNorahAsiri/TTTGameNorahAsiri/ViewController.swift
+++ ios application/TTTGameNorahAsiri/TTTGameNorahAsiri/ViewController.swift
@@ -1,33 +1,17 @@
-//
-//  ViewController.swift
-//  TTTGameNorahAsiri
-//
-//  Created by admin on 06/12/2021.
-//
-
 import UIKit
-
 class ViewController: UIViewController {
-
     @IBOutlet weak var winnerLable: UILabel!
     var player1 : [Int] = []
     var player2 : [Int] = []
     var tttGameTemp : [Int] = []
     var trak : Int = 0
     var winner : Bool = false
-
     @IBAction func restBtn(_ sender: UIButton) {
-      setupUI()
-    }
-    
+      setupUI()}
     @IBOutlet var gameButton: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    setupUI()
-        
-    }
-    
+    setupUI()}
     func setupUI(){
         winnerLable.text = ""
         player2 = []
@@ -36,56 +20,31 @@ class ViewController: UIViewController {
         winner = false
         for aBtn in gameButton {
             aBtn.layer.backgroundColor = UIColor.lightGray.cgColor
-            aBtn.isEnabled = true
-
-        }
-    }
-    
+            aBtn.isEnabled = true}}
     func disable(){
         for aBtn in gameButton {
-            aBtn.isEnabled = false
-        }
-        
-    }
-    
+            aBtn.isEnabled = false}}
     @IBAction func gameAction(_ sender: UIButton) {
-        
         trak += 1
-        
-        
         if (trak % 2 == 0) {
             player2.append(sender.tag)
             sender.backgroundColor = .blue
             winner = tttGame(arr: player2)
             sender.isEnabled = false
-            
             if (winner == true){
                 winnerLable.text = "Congrats Blue Won"
-                disable()
-            }
-            
+                disable()}
         } else {
-            
-            
-            player1.append(sender.tag)
+        player1.append(sender.tag)
             winner = tttGame(arr: player1)
             sender.backgroundColor = .red
             sender.isEnabled = false
-            
             if (winner == true){
                 winnerLable.text = "Congrats Red Won"
-                disable()
-            }
-        }
-        
-        if trak == 9 && winner == false {
-            winnerLable.text = "No Winner!"
-       }
-        
-    }
-    
+                disable()}}
+                if trak == 9 && winner == false {
+            winnerLable.text = "No Winner!"}}
     func tttGame (arr : [Int]) -> Bool {
-        
       if(arr.count >= 3){
        tttGameTemp = arr
        tttGameTemp = tttGameTemp.sorted()
@@ -96,17 +55,7 @@ class ViewController: UIViewController {
           || (tttGameTemp.contains(3) && tttGameTemp.contains(6) && tttGameTemp.contains(9))
           || (tttGameTemp.contains(3) && tttGameTemp.contains(5) && tttGameTemp.contains(7))
           || (tttGameTemp.contains(4) && tttGameTemp.contains(5) && tttGameTemp.contains(6))
-          || (tttGameTemp.contains(7) && tttGameTemp.contains(8) && tttGameTemp.contains(9))
-          {
+          || (tttGameTemp.contains(7) && tttGameTemp.contains(8) && tttGameTemp.contains(9)){
             tttGameTemp = []
-            return true
-          }
-        
-            
-        }
-        
-        return false }
-    
-
-}
-
+            return true}}
+        return false }}
