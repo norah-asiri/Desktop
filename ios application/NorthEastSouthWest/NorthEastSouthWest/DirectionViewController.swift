1eb660bf59860b19a87561ff245048e96daa0a46
norah-asiri <cs.norah2018@gmail.com>
2021-12-16 16:14:36 +0000

    Directions

---
+++ ios application/NorthEastSouthWest/NorthEastSouthWest/DirectionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DirectionViewController.swift
+//  NorthEastSouthWest
+//
+//  Created by admin on 16/12/2021.
+//
+
+import UIKit
+
+class DirectionViewController: UIViewController {
+    var output : String?
+    
+    @IBOutlet weak var directionBtnOutlet: UIButton!
+    
+    @IBAction func directionBtn(_ sender: UIButton) {
+       dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        directionBtnOutlet.setTitle(output, for: .normal)
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+
+}
--- ios application/NorthEastSouthWest/NorthEastSouthWest/ViewController.swift
+++ ios application/NorthEastSouthWest/NorthEastSouthWest/ViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var direction : String = ""
+    
+
+    @IBAction func setDirection(_ sender: UIButton) {
+        direction = sender.titleLabel?.text ?? ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! DirectionViewController
+       
+        // send value
+        destination.output = direction
+    }
+
 
 
 }
