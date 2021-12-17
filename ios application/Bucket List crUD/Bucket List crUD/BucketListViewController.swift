60ac18f83585f7640eca18aeb342df8c1caee462
norah-asiri <cs.norah2018@gmail.com>
2021-12-17 08:25:36 +0000

    cancel

--- ios application/Bucket List crUD/Bucket List crUD/BucketListViewController.swift
+++ ios application/Bucket List crUD/Bucket List crUD/BucketListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class BucketListViewController: UITableViewController {
+class BucketListViewController: UITableViewController , CancelBtnDelegate {
 
     var items = ["Go to the moon", "Eat a candy bar", "Swim in the Amazon", "Ride a motorbike in Tokyo"]
     
@@ -29,6 +29,17 @@ class BucketListViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListItemCell", for: indexPath)
         cell.textLabel?.text = items[indexPath.row]
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let navigationController = segue.destination as! UINavigationController
+        let addItemTableController = navigationController.topViewController as! AddItemTableTableViewController
+        addItemTableController.delegate = self
+        
+    }
+    func cancelBtnPerssed(by controller: UIViewController) {
+        print ("hidden , cancelBtnPressed")
+        dismiss(animated: true, completion: nil)
     }
     }
 
---
+++ ios application/Bucket List crUD/CancelBtnDelgate.swift
@@ -0,0 +1,13 @@
+//
+//  CancelBtnDelgate.swift
+//  Bucket List crUD
+//
+//  Created by admin on 17/12/2021.
+//
+
+import Foundation
+import UIKit
+protocol CancelBtnDelegate : class {
+    
+    func cancelBtnPerssed(by controller : UIViewController)
+}
