698fd739fb142366698a2d030a52e190e45c59f5
norah-asiri <cs.norah2018@gmail.com>
2021-12-21 08:43:49 +0000

    List

---
+++ ios application/MadLibs/MadLibs/StatmentVC.swift
@@ -0,0 +1,26 @@
+//
+//  StstmentVC.swift
+//  MadLibs
+//
+//  Created by admin on 16/12/2021.
+//
+
+import UIKit
+
+class StatmentVC: UIViewController {
+
+    @IBOutlet weak var adjTF: UITextField!
+    @IBOutlet weak var nounTF: UITextField!
+    @IBOutlet weak var verb2TF: UITextField!
+    @IBOutlet weak var verb1TF: UITextField!
+    @IBAction func submitBtn(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- ios application/MadLibs/MadLibs/ViewController.swift
+++ ios application/MadLibs/MadLibs/ViewController.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var statmentLB: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
