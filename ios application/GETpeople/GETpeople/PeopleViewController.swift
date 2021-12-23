dd3870ef2bed8fcbc0c0c02a882ea3d722de177b
norah-asiri <cs.norah2018@gmail.com>
2021-12-23 13:35:44 +0000

    Update PeopleViewController.swift

--- ios application/GETpeople/GETpeople/PeopleViewController.swift
+++ ios application/GETpeople/GETpeople/PeopleViewController.swift
@@ -46,8 +46,8 @@ class PeopleViewController: UIViewController {
     func parseLogic (data : Data) {
         do {
             
-            let dic = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
-             print (dic)
+            let dict = try JSONSerialization.jsonObject(with: data, options: []) as! [String:Any]
+             print (dict)
              if let results = dic["result"] as? [[String : Any]]{
                  for result in results {
                     parsePeopleInfo(dict: result)
@@ -93,90 +93,4 @@ extension PeopleViewController : UITableViewDataSource {
     
 }
 
-/*
-    // create a URLSession to handle the request tasks
-    let session = URLSession.shared
-    // create a "data task" to make the request and run the completion handler
-    let task = session.dataTask(with: url!, completionHandler: {
-        // see: Swift closure expression syntax
-        data, response, error in
-        print("in here")
-        // see: Swift nil coalescing operator (double questionmark)
-        print(data ?? "no data") // the "no data" is a default value to use if data is nil
-    })
-    // execute the task and wait for the response before
-    // running the completion handler. This is async!
-    task.resume()
-}
- */
-
-
-/*
-// Hardcoded data for now
-var people = ["Luke Skywalker", "Leia Organa", "Han Solo", "C-3PO", "R2-D2"]
-override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    let peopleData = NSData.init(contentsOfFile: "People.json")
-    let dic = JSONSerialization.jsonObject(with: peopleData , options:[])
-}
-override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-}
-override func numberOfSections(in tableView: UITableView) -> Int {
-    // if we return - sections we won't have any sections to put our rows in
-    return 1
-}
-override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // return the count of people in our data array
-    return people.count
-}
-override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    // Create a generic cell
-    let cell = UITableViewCell()
-    // set the default cell label to the corresponding element in the people array
-    cell.textLabel?.text = people[indexPath.row]
-    // return the cell so that it can be rendered
-    return cell
-}
-}
-
-/*
-var people = [String]()
-
-override func viewDidLoad() {
-super.viewDidLoad()
-// Do any additional setup after loading the view.
-
-/*
-let url = URL(string: "")
-let session = URLSession.shared
-let task = session.dataTask(with: url!, completionHandler: {
-    data ,response , error in
-    do {
-        if let jsonResult = try JSONSerialization.jsonObject(with: data!, options:
-           JSONSerialization ReadingOptions.mutableContainers) as? NSDictionary {
-            
-       if let results = jsonResult["results"] as? NSArray {
-           
-           for person in results {
-               let personDic =   person as! NSDictionary
-               self.people.append(personDic["name"]! as! String)
-           }
-            
-        }
-            
-        } catch {
-            print (error)
-        }
-
-})
-}
-
-
-
-}
-*/
-*/
-*/
 
