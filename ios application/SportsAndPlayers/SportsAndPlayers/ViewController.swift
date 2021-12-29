5ca4dd01008b0526ad4de67bdf2c45e88df9845f
norah-asiri <cs.norah2018@gmail.com>
2021-12-29 22:19:43 +0000

    add alert

--- ios application/SportsAndPlayers/SportsAndPlayers/ViewController.swift
+++ ios application/SportsAndPlayers/SportsAndPlayers/ViewController.swift
@@ -16,5 +16,25 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func addAlert(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title:"New Sport" , message:"Add a new Sport", preferredStyle: .alert)
+        
+        alert.addTextField {(textField:UITextField) in
+            textField.placeholder = "New Sport"
+        }
+        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: { (action : UIAlertAction) in
+            if let textField = alert.textFields?.first {
+                if textField.text == "" {
+                    print("You hava to enter somthing first")
+                }else {
+                    print("you enter \(textField.text!)")
+                }
+            }
+            
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style : .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
 }
 
