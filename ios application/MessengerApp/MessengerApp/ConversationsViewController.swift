dd431913799ba60ffe6f88095961cefa751773b8
norah-asiri <58185376+norah-asiri@users.noreply.github.com>
2022-01-05 00:05:51 +0000

    MessengerApp

---
+++ ios application/MessengerApp/MessengerApp/ConversationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConversationsViewController.swift
+//  MessengerApp
+//
+//  Created by administrator on 04/01/2022.
+//
+
+import UIKit
+
+
+class ConversationsViewController: UIViewController {
+    // check to see if user is signed in using ... user defaults
+    // they are, stay on the screen. If not, show the login screen
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        if !isLoggedIn {
+            // present login view controller
+            
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        }
+    }
+}
