7f50f2f96c4ec61eead488f049dbb406f7982af6
norah-asiri <58185376+norah-asiri@users.noreply.github.com>
2022-01-08 06:23:03 +0000

    MessengerApp

--- ios application/MessengerApp/MessengerAppIOS/MessengerAppIOS/Controllers/LogReg/RegisterViewController.swift
+++ ios application/MessengerApp/MessengerAppIOS/MessengerAppIOS/Controllers/LogReg/RegisterViewController.swift
@@ -6,89 +6,37 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    
+    @IBAction func signUpBtn(_ sender: Any) {
+        // SINGUP
+           // Firebase Login / check to see if email is taken
+           // try to create an account
+     Auth.auth().createUser(withEmail: email.text!, password: password.text!, completion: { authResult , error  in
+               guard let result = authResult, error == nil else {
+                   print("Error creating user")
+                   return
+               }
+               let user = result.user
+               print("Created User: \(user)")
+           })
+    }
+    
+   
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//   // private let scrollView: UIScrollView = {
-//            let scrollView = UIScrollView()
-//            scrollView.clipsToBounds = true
-//       //     return scrollView
-//      //  }()
-//
-//      //  private let emailField: UITextField = {
-//            let emailField = UITextField()
-//        emailField.placeholder = "Email Address..."
-//          //  ...
-//        //    return field
-//        //}()
-//      //  private let passwordField: UITextField = {
-//            let passwordField = UITextField()
-//            //   ...
-//        passwordField.backgroundColor = .white
-//        passwordField.isSecureTextEntry = true
-//         //   return field
-//      //  }()
-//       // private let firstNameField: UITextField = {
-//            let firstNameField = UITextField()
-//          //  ...
-//          //  return field
-//       // }()
-//       // private let lastNameField: UITextField = {
-//            let lastNameField = UITextField()
-//              // ....
-//            // return field
-//    //    }()
-//
-//
-//        //private let registerButton: UIButton = {
-//            let button = UIButton()
-//            button.setTitle("Register", for: .normal)
-//            button.backgroundColor = .systemGreen
-//            button.setTitleColor(.white, for: .normal)
-//            button.layer.cornerRadius = 12
-//            button.layer.masksToBounds = true
-//            button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
-//          //  return button
-//      //  }()
-//
-//     //   private let imageView: UIImageView = {
-//
-//            // Create the UIImageView object with frame
-//                let imageView = UIImageView()
-//                imageView.frame = CGRect(x: 50, y: 50, width: 100, height: 100)
-//            imageView.image = UIImage(systemName: "person")
-//            imageView.tintColor = .gray
-//
-//                // set the image to aspect fit
-//        //    return imageView
-//        //}()
-//
-//
-////
-////            let label = UILabel()
-////                label.frame = CGRect(x: 50, y: 50, width: 100, height: 30)
-////                label.text = "Test Label"
-////
-////                // Add the label to the view controller's root view
-////                self.view.addSubview(label)
-//
-////            emailField.delegate = self
-////            passwordField.delegate = self
-//
-//            // add subviews
-//
-//            scrollView.addSubview(imageView)
-//            scrollView.addSubview(firstNameField)
-//            scrollView.addSubview(lastNameField)
-//            scrollView.addSubview(emailField)
-//            scrollView.addSubview(passwordField)
-//            scrollView.addSubview(button)
-//            self.view.addSubview(scrollView)
+        
+        
         }
     
 }
--- ios application/MessengerApp/MessengerAppIOS/MessengerAppIOS/Controllers/ViewController.swift
+++ ios application/MessengerApp/MessengerAppIOS/MessengerAppIOS/Controllers/ViewController.swift
@@ -6,9 +6,27 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
+    
+    @IBAction func logInBtn(_ sender: UIButton) {
+    
+    // LOGIN  Firebase Login
+        Auth.auth().signIn(withEmail: email.text!, password: password.text!, completion: { authResult, error in
+       guard let result = authResult, error == nil else {
+           print("Failed to log in user with email \(self.email.text!)")
+           return
+       }
+       let user = result.user
+       print("logged in user: \(user)")
+   })
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
